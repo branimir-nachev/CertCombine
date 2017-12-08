2d2008662f21669166537c826441c083a45999b6
Branimir Nachev <branimir_nachev@me.com>
2017-12-08 14:20:07 +0200

    Initial Commit

---
+++ CertCombine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CertCombine
+//
+//  Created by Branimir Nachev on 8.12.17 г..
+//  Copyright © 2017 г. Branimir Nachev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
